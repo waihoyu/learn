8c689cab8ad0b4d0187f50725a61269da17c250e
waihoyu <13701343809@qq.com>
2018-07-18 22:00:06 +0800

    init this project
    init this project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class = "main">
+    <button v-on:click="show = !show">
+      Toggle
+    </button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+      <div  class="food">  
+          111
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+　　export default{
+    data() {
+      return {
+        show: true
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  .food 
+      position fixed
+      width 400px
+      height 400px
+      left 200px
+      top 0
+      bottom 48px
+      z-index 30  
+      background blue
+      transform all 0.3s linear
+
+  .fade-transition
+    transition: all 0.2s linear
+    transform: translate3d(0, 0, 0)
+  .fade-enter, .fade-leave-to
+    transition: all 0.2s linear
+    transform: translate3d(100%, 0, 0)
+    background yellow 
+</style>
